a7e5af4c746144585b505a44407b97aed152abb7
kev <31861128+piyoki@users.noreply.github.com>
2025-05-16 12:26:57 -0400

    feat(lua/plugins): add checkmate (#8)
    * patch(core/keymap): update toc shortcut
    
    * feat(lua/plugins): add checkmate
    
    * patch(lua/config): update interface look
    
    * patch(init): override module loading order
    
    * patch(lua/plugins): specify checkmate tag

---
+++ lua/config/todo.lua
@@ -0,0 +1,61 @@
+local M = {}
+local todo = require('checkmate')
+local util = require('checkmate.util')
+
+local colors = {
+  purple = '#b4befe',
+  green = '#96de7a',
+  yellow = '#f7ffbc',
+}
+
+function M.setup()
+  -- Ref: https://github.com/bngarren/checkmate.nvim
+  todo.setup({
+    files = { 'TODO', 'PLANNING', 'PROPOSAL', '*meeting*' }, -- matches TODO.md, PLANNING.md, PROPOSAL.md, and any file with meeting in the name
+    -- Default keymappings
+    keys = {
+      ['<leader>tc'] = 'create', -- Create todo item
+    },
+    todo_count_recursive = true,
+    todo_count_position = 'eol',
+    style = {
+      list_marker_unordered = {
+        -- Can use util functions to get existing highlight colors and blend them together
+        -- This is one way to integrate with an existing colorscheme
+        fg = util.blend(
+          util.get_hl_color('Normal', 'fg', '#bbbbbb'),
+          util.get_hl_color('Normal', 'bg', '#222222'),
+          0.2
+        ),
+      },
+      list_marker_ordered = {
+        fg = util.blend(
+          util.get_hl_color('Normal', 'fg', '#bbbbbb'),
+          util.get_hl_color('Normal', 'bg', '#222222'),
+          0.5
+        ),
+      },
+      -- Unchecked todo items
+      unchecked_marker = { fg = '#bfbfbf', bold = true }, -- The marker itself
+      unchecked_main_content = { fg = '#ffffff' }, -- Style settings for main content: typically the first line/paragraph
+      unchecked_additional_content = { fg = '#dddddd' }, -- Settings for additional content
+
+      -- Checked todo items
+      checked_marker = { fg = colors.green, bold = true }, -- The marker itself
+      checked_main_content = { fg = '#aaaaaa', strikethrough = true }, -- Style settings for main content: typically the first line/paragraph
+      checked_additional_content = { fg = '#aaaaaa' }, -- Settings for additional content
+    },
+    metadata = {
+      priority = { key = '<leader>tp' },
+      started = { key = '<leader>ts', style = { fg = '#f7ffbc' } },
+      done = {
+        key = '<leader>tt',
+        on_remove = function(todo_item)
+          require('checkmate').set_todo_item(todo_item, 'unchecked')
+        end,
+      },
+    },
+  })
+end
+
+return M
