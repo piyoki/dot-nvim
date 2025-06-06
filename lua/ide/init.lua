0c1d7e2e235720c3fb625073908086251b529ee3
kev <31861128+piyoki@users.noreply.github.com>
2025-06-06 10:05:00 -0400

    refactor(lua/ide): rename copilot.lua -> copilotchat.lua

--- lua/ide/init.lua
+++ lua/ide/init.lua
@@ -9,7 +9,7 @@ function M.setup()
   require('ide.treesitter').setup()
   require('ide.lsp').setup()
   require('ide.cmp').setup()
-  require('ide.copilot').setup()
+  require('ide.copilotchat').setup()
 end
 
 return M
