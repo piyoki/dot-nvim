65e5dc4b5eafa973187d06056b179937df09e590
kev <31861128+piyoki@users.noreply.github.com>
2025-10-31 16:33:19 -0400

    patch(lua/core/keymap): update FTerm and Lazygit mapping

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -90,7 +90,7 @@ function M.setup()
   -- FloatTerm
   utils.map('n', '<C-x>', ":lua require('FTerm').toggle()<CR>", opts) -- (N) toggle Fterm
   utils.map('t', '<C-x>', "<C-\\><C-n><CMD>lua require('FTerm').toggle()<CR>") -- (T) Close out Fterm
-  -- utils.map('t', '<C-g>', "q<C-\\><C-n><CMD>lua require('FTerm').toggle()<CR>") -- (T) Close out Lazygit Fterm
+  utils.map('t', '<C-g>', 'q<C-\\><C-n>aexit<CR>') -- (T) Close out Fterm with exit command
 
   -- Commentary
   utils.map('n', '<LEADER>/', ':Commentary<CR>') -- (N) Toggle Comment
@@ -127,8 +127,8 @@ function M.setup()
     { '<Leader>gdc', '<cmd>DiffviewClose<CR>', desc = 'DiffviewClose' },
     { '<Leader>gdf', '<cmd>DiffviewFileHistory<CR>', desc = 'DiffviewFileHistory' },
     { '<Leader>gb', '<cmd>GitBlameToggle<CR>', desc = 'GitBlameToggle' },
-    { '<Leader>gg', '<cmd>LazyGit<CR>', desc = 'LazyGit' },
-    -- { '<Leader>gg', ':lua require("FTerm").toggle()<CR>lazygit<CR>', desc = 'LazyGit' },
+    -- { '<Leader>gg', '<cmd>LazyGit<CR>', desc = 'LazyGit' },
+    { '<Leader>gg', ':lua require("FTerm").toggle()<CR>lazygit<CR>', desc = 'LazyGit' },
   })
 
   -- Overwrite default settings
