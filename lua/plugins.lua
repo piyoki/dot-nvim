64e15af44f4446575a8fa5a7a088972b0aebcb47
kev <31861128+piyoki@users.noreply.github.com>
2025-11-02 11:46:16 -0500

    patch(lua/plugins): update checkmate.nvim to v0.11.2

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -86,7 +86,7 @@ return require('packer').startup(function()
   use({ 'wakatime/vim-wakatime' })
   use({ 'tamago324/lir.nvim' })
   use({ 'rmagatti/auto-session' })
-  use({ 'bngarren/checkmate.nvim' })
+  use({ 'bngarren/checkmate.nvim', tag = 'v0.11.2' })
 
   -- searching & filtering
   use({ 'nvim-telescope/telescope.nvim' })
